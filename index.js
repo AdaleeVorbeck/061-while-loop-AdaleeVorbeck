01b7ebce25af1383a8aa850170be97067a1c214b
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-12 20:29:55 +0000

    WE SLEEP UNTIL THE SUN GOES DOWN

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ let todos = ['Laundry', 'Dishes', 'Clean Desk', 'Sweep Floor'];
 function markAsDone(todos) {
     while (todos < 5){
     todos ++;
-    }
+    todos[i] = 'done - ' + todos[i];
+ }
+ return todos;
 }
 
 document.getElementById('result').innerHTML = markAsDone(todos);
+
+//should create a function named markAsDone that takes an array of todos and returns the modified array
+//should loop through the todos, using a while loop, and prepend the string 'done - ' to each todo description
