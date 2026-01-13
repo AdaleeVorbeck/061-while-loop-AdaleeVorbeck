70daf6f3cf1ac1d8a2342dedad835d870b268e13
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-13 20:09:04 +0000

    I dont know the current song well enough to put lyrics :(

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function markAsDone(todos) {
     while (i < todos.length){
     todos[i] = "done - " + todos[i];
     i++;
- }
- return todos.join('<br>');
+    }
+    return todos;
 }
 
-document.getElementById('result').innerHTML = markAsDone(todos);
+document.getElementById('result').innerHTML = markAsDone(todos).join('<br>');
 
-
+//should create a function named markAsDone that takes an array of todos and returns the modified array
 //should loop through the todos, using a while loop, and prepend the string 'done - ' to each todo description
